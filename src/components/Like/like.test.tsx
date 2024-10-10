c1fee5284cb8a62a5b065f6d5c63d8593a37988d
Katie Stamm <wishingstar0017@yahoo.com>
2024-10-10 09:34:40 -0600

    add additional test

--- src/components/Like/like.test.tsx
+++ src/components/Like/like.test.tsx
@@ -14,3 +14,14 @@ test('changes button text to liked after click', () => {
   const likedButtonElement = screen.getByText("Liked");
   expect(likedButtonElement).toBeInTheDocument();
 });
+
+test('changes button text back to like after second click', () => {
+  render(<Like />);
+  const likeButtonElement = screen.getByText("Like");
+  fireEvent.click(likeButtonElement);
+  const likedButtonElement = screen.getByText("Liked");
+  expect(likedButtonElement).toBeInTheDocument();
+  fireEvent.click(likedButtonElement);
+  const likeButtonElementAfterSecondClick = screen.getByText("Like");
+  expect(likeButtonElementAfterSecondClick).toBeInTheDocument();
+});
